a01b59e9fc08b30d58863903aa5043e1db5f7112
ycwn <ycwn@hyperion>
2022-07-18 23:01:58 +0300

    ~ Implemented ADC functions.

--- lib/core16/analog/adc.c
+++ lib/core16/analog/adc.c
@@ -2,5 +2,115 @@
 
 #include "core/core.h"
 
-#include "adc.h"
+#include "analog/adc.h"
+
+
+
+void adc_mode(word mode, byte channels)
+{
+
+	PIR1bits.ADIF = 0;
+	PIE1bits.ADIE = 0;
+	IPR1bits.ADIP = 0;
+
+	ADCON0 = 0;
+	ADCON1 = 0;
+	ADCON2 = 0;
+
+	if ((mode & ADC_ENABLE) == 0)
+		return;
+
+	if (mode & ADC_CLK_FOSC_4)  ADCON2bits.ADCS2 = 1;
+	if (mode & ADC_CLK_FOSC_32) ADCON2bits.ADCS1 = 1;
+	if (mode & ADC_CLK_FOSC_8)  ADCON2bits.ADCS0 = 1;
+
+	if (mode & ADC_TACQ_8CY) ADCON2bits.ACQT2 = 1;
+	if (mode & ADC_TACQ_4CY) ADCON2bits.ACQT1 = 1;
+	if (mode & ADC_TACQ_2CY) ADCON2bits.ACQT0 = 1;
+
+	if (mode & ADC_FORMAT_RJUST) ADCON2bits.ADFM = 1;
+
+	if ((channels & 8) == 0) ADCON1bits.PCFG3 = 1;
+	if ((channels & 4) == 0) ADCON1bits.PCFG2 = 1;
+	if ((channels & 2) == 0) ADCON1bits.PCFG1 = 1;
+	if ((channels & 1) == 0) ADCON1bits.PCFG0 = 1;
+
+	if (mode & ADC_NVREF_EXT) ADCON1bits.VCFG1 = 1;
+	if (mode & ADC_PVREF_EXT) ADCON1bits.VCFG0 = 1;
+
+	if (mode & ADC_INTERRUPT) {
+
+		PIE1bits.ADIE = 1;
+
+		if (mode & ADC_INTERRUPT_HIGH)
+			IPR1bits.ADIP = 1;
+
+	}
+
+	ADCON0bits.ADON = 1;
+
+}
+
+
+
+word adc_read(byte ch)
+{
+
+	if (ch != 255) {
+
+		if (ch & 8) ADCON0bits.CHS3 = 1;
+		if (ch & 4) ADCON0bits.CHS2 = 1;
+		if (ch & 2) ADCON0bits.CHS1 = 1;
+		if (ch & 1) ADCON0bits.CHS0 = 1;
+
+	}
+
+	PIR1bits.ADIF = 0;
+	ADCON0bits.GO = 1;
+
+	while (ADCON0bits.GO);
+
+	return WORD2(ADRESL, ADRESH);
+
+}
+
+
+
+void adc_select(byte ch)
+{
+
+	if (ch & 8) ADCON0bits.CHS3 = 1;
+	if (ch & 4) ADCON0bits.CHS2 = 1;
+	if (ch & 2) ADCON0bits.CHS1 = 1;
+	if (ch & 1) ADCON0bits.CHS0 = 1;
+
+}
+
+
+
+void adc_start()
+{
+
+	PIR1bits.ADIF = 0;
+	ADCON0bits.GO = 1;
+
+}
+
+
+
+byte adc_ready()
+{
+
+	return ADCON0bits.GO == 0;
+
+}
+
+
+
+word adc_sample()
+{
+
+	return WORD2(ADRESL, ADRESH);
+
+}
 
--- lib/core16/analog/adc.h
+++ lib/core16/analog/adc.h
@@ -1,8 +1,56 @@
 
 
-#ifndef __ADC_H
-#define __ADC_H
+#ifndef ANALOG_ADC_H
+#define ANALOG_ADC_H
 
+
+enum {
+
+	ADC_DISABLE = 0 << 0,
+	ADC_ENABLE  = 1 << 0,
+
+	ADC_POLLING   = 0 << 1,
+	ADC_INTERRUPT = 1 << 1,
+
+	ADC_INTERRUPT_LOW  = 0 << 2,
+	ADC_INTERRUPT_HIGH = 1 << 2,
+
+	ADC_NVREF_GND = 0 << 4,
+	ADC_NVREF_EXT = 1 << 4,
+
+	ADC_PVREF_VCC = 0 << 5,
+	ADC_PVREF_EXT = 1 << 5,
+
+	ADC_FORMAT_LJUST = 0 << 6,
+	ADC_FORMAT_RJUST = 1 << 6,
+
+	ADC_TACQ_0CY  = 0 << 8,
+	ADC_TACQ_2CY  = 1 << 8,
+	ADC_TACQ_4CY  = 2 << 8,
+	ADC_TACQ_6CY  = 3 << 8,
+	ADC_TACQ_8CY  = 4 << 8,
+	ADC_TACQ_12CY = 5 << 8,
+	ADC_TACQ_16CY = 6 << 8,
+	ADC_TACQ_20CY = 7 << 8,
+
+	ADC_CLK_FOSC_2  = 0 << 12,
+	ADC_CLK_FOSC_8  = 1 << 12,
+	ADC_CLK_FOSC_32 = 2 << 12,
+	ADC_CLK_FOSC_4  = 4 << 12,
+	ADC_CLK_FOSC_16 = 5 << 12,
+	ADC_CLK_FOSC_64 = 6 << 12,
+	ADC_CLK_FRC     = 7 << 12
+
+};
+
+
+void adc_mode(word mode, byte channels);
+word adc_read(byte ch);
+
+void adc_select(byte ch);
+void adc_start();
+byte adc_ready();
+word adc_sample();
 
 
 #endif
